5cfc5bbf5a51972152d20912519cf756fea830b2
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-25 19:56:59 +0530

    X_train & y_train

--- ntlk_utils.py
+++ ntlk_utils.py
@@ -1,5 +1,6 @@
 import nltk
 nltk.download('punkt')
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -12,5 +13,10 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence,all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zero(len(all_words), dtype = np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
 
+    return bag
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import json
 from ntlk_utils import tokenize, stem, bag_of_words
+import numpy as np
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -18,4 +19,19 @@ for intent in intents['intents']:
 
 
 ignore_words = ['?', '!', '.', ',']
-print(all_words)
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence,all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
