e6f7238e5e931e780541cdaf0f79835c432b3010
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-25 16:04:03 +0530

    NTLK

---
+++ ntlk_utils.py
@@ -0,0 +1,20 @@
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+# breaks sentences into words
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence,all_words):
+    pass
+
+a= "How do you live ?"
+print(a)
+a = tokenize(a)
+print(a)
