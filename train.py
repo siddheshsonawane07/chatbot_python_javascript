323baae706c79598765544ef2dd74c2159e48c50
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-26 21:08:03 +0530

    part 4 started

--- train.py
+++ train.py
@@ -29,6 +29,7 @@ tags = sorted(set(tags))
 
 X_train = []
 y_train = []
+
 for (pattern_sentence, tag) in xy:
     bag = bag_of_words(pattern_sentence,all_words)
     X_train.append(bag)
@@ -39,6 +40,14 @@ for (pattern_sentence, tag) in xy:
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
+# Hyperparameters
+batch_size = 64
+hidden_size = 8
+output_size = len(tags)
+input_size = len(X_train[0])
+learning_rate = 0.001
+num_epochs = 1000
+
 class ChatDataset(Dataset):
     def __init__(self):
         self.n_samples = len(X_train)
@@ -46,22 +55,14 @@ class ChatDataset(Dataset):
         self.y_data = y_train
 
     def __getitem__(self, index):
-        return self.x_data[idx], self.y_data[idx]
+        return self.x_data[index], self.y_data[index]
 
     def __len__(self):
         return self.n_samples
-    
-# Hyperparameters
-batch_size = 8
-hidden_size = 8
-output_size = len(tags)
-input_size = len(X_train[0])
-learning_rate = 0.001
-num_epochs = 1000
 
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2)
+train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 0)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = NeuralNet(input_size, hidden_size, output_size)
@@ -71,9 +72,9 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
 
 for epoch in range(num_epochs):
-    for (words, labels) in train_loader"
-    words = words.to(device)
-    labels = labels.to(device)
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype = torch.long).to(device)
 
     # forward 
     outputs = model(words)
