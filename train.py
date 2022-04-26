e70d9ca39b2b06023fdbe2e9af5d2be0ecd15fa0
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-26 20:29:36 +0530

    all set for burning my gpu

--- train.py
+++ train.py
@@ -26,7 +26,6 @@ ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
 
 X_train = []
 y_train = []
@@ -57,10 +56,36 @@ batch_size = 8
 hidden_size = 8
 output_size = len(tags)
 input_size = len(X_train[0])
-print(input_size, len(all_words))
-print(output_size, tags)
+learning_rate = 0.001
+num_epochs = 1000
+
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2)
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = NeuralNet(input_size, hidden_size, output_size)
+
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader"
+    words = words.to(device)
+    labels = labels.to(device)
+
+    # forward 
+    outputs = model(words)
+    loss = criterion(outputs,labels)
+
+    # backward and optimizer step
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if(epoch + 1) % 100 == 0:
+        print(f'epoch{epoch+1}/{num_epochs},loss= {loss.item():.4f}')
+
+print(f'final loss,loss= {loss.item():.4f}')
+
